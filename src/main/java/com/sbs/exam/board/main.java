717f68a275fcfe14d3668180de02cfbf48d1d293
geniejini00056 <jiny9010@gmail.com>
2024-08-16 15:35:13 +0900

    시작 알림 메시지 출력

--- src/main/java/com/sbs/exam/board/main.java
+++ src/main/java/com/sbs/exam/board/main.java
@@ -2,6 +2,9 @@ package com.sbs.exam.board;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("== 자바 텍스트 게시판 ==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.println("명령");
+        System.out.println("== 프로그램 종료 ==");
     }
 }
