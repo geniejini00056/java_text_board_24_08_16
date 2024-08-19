26dfd0932b05621dfa9dad90f39617874b572865
geniejini00056 <jiny9010@gmail.com>
2024-08-19 11:52:20 +0900

    usr/article/write 명령 입력 받기

--- src/main/java/com/sbs/exam/board/main.java
+++ src/main/java/com/sbs/exam/board/main.java
@@ -14,12 +14,29 @@ public class main {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("exit")){
+            if(cmd.equals("/usr/article/write")){
+                System.out.println("==게시물 등록==");
+
+                System.out.printf("제목: ");
+                String title = sc.nextLine();
+
+                System.out.printf("내용: ");
+                String content = sc.nextLine();
+
+                int id = 1;
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+
+            }
+
+            else if(cmd.equals("exit")){
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
-
-            System.out.printf("입력된 명령어 : %s\n", cmd);
+            
+            else{
+                System.out.println("명령어를 잘못 입력하셨습니다.");
+            }
         }
 
         sc.close();
