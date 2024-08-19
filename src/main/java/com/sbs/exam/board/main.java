87199da3e3d94e08c4bce7f38bb351d3bc4dd505
geniejini00056 <jiny9010@gmail.com>
2024-08-19 11:30:33 +0900

    명령어 입력 받기

--- src/main/java/com/sbs/exam/board/main.java
+++ src/main/java/com/sbs/exam/board/main.java
@@ -1,10 +1,27 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
         System.out.println("== 자바 텍스트 게시판 ==");
         System.out.println("== 프로그램 시작 ==");
-        System.out.println("명령");
-        System.out.println("== 프로그램 종료 ==");
+        while (true) {
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("exit")){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.printf("입력된 명령어 : %s\n", cmd);
+        }
+
+        sc.close();
     }
 }
